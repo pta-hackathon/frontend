c933535f84168d6da0f52a3b2f65e88ee22e0263
Josias Brenner <Josias.Brenner@pta.de>
2024-04-28 15:44:05 +0200

    wip

--- src/Ticket.tsx
+++ src/Ticket.tsx
@@ -21,13 +21,13 @@ const Ticket = () => {
 
   return (
     <div className="flex flex-col gap-4 rounded border bg-white px-4 py-2">
-      <h3 className="text-3xl font-semibold">{ticket?.text || "Waiting for Ticket..."}</h3>
+      <h3 className="text-xl font-semibold">{ticket?.text || "Waiting for Ticket..."}</h3>
       {ticket ? (
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin turpis quam, efficitur in urna sed, posuere
-          faucibus ex. Vivamus maximus nulla at ligula interdum consectetur. Aliquam porttitor efficitur lectus id
-          consequat. Suspendisse aliquet, libero vitae pharetra fermentum, ante metus aliquet ante, nec lacinia tortor
-          nibh vel leo.
+          Für die Fehlersuche und -behebung in Computersystemen werden häufig spezielle Tools und Skripte benötigt. Eine
+          solche Methode, die für viele IT-Experten entscheidend ist, ist die Verwendung von Minidump-Dateien. Diese
+          Dateien bieten eine Momentaufnahme des aktiven Speichers, wenn ein Computer abstürzt, und liefern unschätzbare
+          Daten für die Analyse nach dem Absturz.
         </p>
       ) : (
         <div className="flex h-36 items-center justify-center text-3xl">
